aafdc5236f20f1dc568957ffd5f5c2936e45b7ca
Annaguzel <you@example.com>
2020-03-10 16:47:02 +0200

    Done

--- src/AuthorCard.js
+++ src/AuthorCard.js
@@ -1,8 +1,6 @@
 import React from "react";
-
-const AuthorCard = props => {
+function AuthorCard(props) {
   const author = props.author;
-  const authorName = `${author.first_name} ${author.last_name}`;
   return (
     <div className="col-lg-4 col-md-6 col-12">
       <div className="card">
@@ -10,18 +8,17 @@ const AuthorCard = props => {
           <img
             className="card-img-top img-fluid"
             src={author.imageUrl}
-            alt={authorName}
+            alt={author.first_name + " " + author.last_name}
           />
         </div>
         <div className="card-body">
           <h5 className="card-title">
-            <span>{authorName}</span>
+            <span>{author.first_name + " " + author.last_name}</span>
           </h5>
           <small className="card-text">{author.books.length} books</small>
         </div>
       </div>
     </div>
   );
-};
-
+}
 export default AuthorCard;
--- src/AuthorList.js
+++ src/AuthorList.js
@@ -1,17 +1,14 @@
 import React from "react";
 import AuthorCard from "./AuthorCard";
-
-const AuthorList = props => {
-  const authorCards = props.authors.map(author => (
+function AuthorList(props) {
+  const authorList = props.authors.map(author => (
     <AuthorCard key={author.first_name + author.last_name} author={author} />
   ));
-
   return (
     <div className="authors">
       <h3>Authors</h3>
-      <div className="row">{authorCards}</div>
+      <div className="row">{authorList}</div>
     </div>
   );
-};
-
+}
 export default AuthorList;
